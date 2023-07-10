8c47f6bda1cfd5670c55cfb34787be9b713fa95b
dheimgartner <d.heimgartners@gmail.com>
2023-07-10 16:32:07 +0200

    added ...

--- R/theme_ivt.R
+++ R/theme_ivt.R
@@ -7,12 +7,13 @@
 #' ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length)) +
 #'   geom_point() +
 #'   theme_ivt()
-theme_ivt <- function() {
+theme_ivt <- function(...) {
   ivt_theme <-
     ggplot2::theme_light() +
     ggplot2::theme(legend.position = "right",
                    strip.background = element_rect(fill = "white"),
-                   strip.text = element_text(colour = "black"))
+                   strip.text = element_text(colour = "black"),
+                   ...)
 
   ivt_theme
 }
